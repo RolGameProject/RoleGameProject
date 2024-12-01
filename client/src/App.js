0d8dd31cbea0f3bb03e64de17aa0512a7edb054a
RolGameProject <rolgameproject@gmail.com>
2024-12-01 19:28:08 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import axios from 'axios';
+// import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
+// import axios from 'axios';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -13,36 +13,36 @@ import GameRoom from './pages/GameRoom';
 // import Interaction from './pages/Interaction';
 // import TurnManagement from './pages/TurnManagement';
 import Navbar from './components/Navbar';
-import LoadingSpinner from './components/LoadingSpinner';
+// import LoadingSpinner from './components/LoadingSpinner';
 import './styles/styles.css';
 
-const BACKENDURL=process.env.REACT_APP_BACKEND_URL;
+// const BACKENDURL=process.env.REACT_APP_BACKEND_URL;
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [loading, setLoading] = useState(true);
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+  // const [loading, setLoading] = useState(true);
 
   // Verificar si el usuario está autenticado
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const response = await axios.get(BACKENDURL + '/api/auth/success', { withCredentials: true });
-        setIsAuthenticated(response.data.authenticated);
-      } catch (error) {
-        console.error('Error al verificar autenticación:', error);
-        setIsAuthenticated(false);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const checkAuth = async () => {
+  //     try {
+  //       const response = await axios.get(BACKENDURL + '/api/auth/success', { withCredentials: true });
+  //       setIsAuthenticated(response.data.authenticated);
+  //     } catch (error) {
+  //       console.error('Error al verificar autenticación:', error);
+  //       setIsAuthenticated(false);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    checkAuth();
-  }, []);
+  //   checkAuth();
+  // }, []);
 
   // Mostrar el spinner de carga mientras se verifica la autenticación
-  if (loading) {
-    return <LoadingSpinner />;
-  }
+  // if (loading) {
+  //   return <LoadingSpinner />;
+  // }
 
   return (
     <>
