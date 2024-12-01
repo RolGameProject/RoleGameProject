2a1ebe74b5e9ca424cbe3463bd31243325bc5a27
RolGameProject <rolgameproject@gmail.com>
2024-12-01 19:20:38 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 
 // import React, { useState, useEffect } from 'react';
 
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Route, Routes } from 'react-router-dom';
 
 // import axios from 'axios';
 
