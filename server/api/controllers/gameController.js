fb07aa7fa234842def8e7511e948bf163e302957
RolGameProject <rolgameproject@gmail.com>
2024-12-02 23:27:22 +0100

    Update gameController.js

--- server/api/controllers/gameController.js
+++ server/api/controllers/gameController.js
@@ -9,7 +9,11 @@ const ensureAuthenticated = (req, res, next) => {
     console.log('req.isAuthenticated:', req.isAuthenticated());
     console.log('req.user:', req.user);
     console.log('req.session:', req.session);
-
+    
+    app.use((req, res, next) => {
+        console.log('Sesión actual: ', req.session);  // Verifica que la sesión contiene el ID del usuario
+        next();
+    });
     // En modo de prueba, omitimos la verificación de autenticación
     if (process.env.NODE_ENV === 'test') {
         return next();
