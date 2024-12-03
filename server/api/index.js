3c470a268c8810a04c636a16d20f783a41be0b08
RolGameProject <rolgameproject@gmail.com>
2024-12-03 10:16:55 +0100

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -14,7 +14,7 @@ const passport = require('passport');
 require('./config/passportConfig');
 const authRoutes = require('./routes/authRoutes');
 const characterRoutes = require('./routes/characterRoutes');
-const cookieParser = require('cookie-parser');
+// const cookieParser = require('cookie-parser');
 
 const FRONTEND = process.env.REACT_APP_FRONTEND_URL;
 
@@ -61,7 +61,7 @@ console.log('CORS configurado con origen:', FRONTEND);
 // console.log('Sesión configurada correctamente');
 
 // Usamos cookie-parser para manejar las cookies
-app.use(cookieParser());
+// app.use(cookieParser());
 
 // Configuramos Express para que acepte JSON y formularios codificados
 app.use(express.json());
