fe08d0d7712ebcbb6b6ce1265d2187020363f58a
RolGameProject <rolgameproject@gmail.com>
2024-12-04 10:44:41 +0100

    Update GameRoom.js

--- client/src/pages/GameRoom.js
+++ client/src/pages/GameRoom.js
@@ -32,7 +32,7 @@ function GameRoom() {
 
 
   const { gameId, discordLink } = location.state || {};
-  console.log('location.state en GameRoom: ', location.state;
+  console.log('location.state en GameRoom: ', location.state);
 
    const fetchGameDetails = useCallback(async () => {
     try {
