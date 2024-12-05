532448df1cb017551c0f680599aa2a45a254daea
RolGameProject <rolgameproject@gmail.com>
2024-12-05 10:25:47 +0100

    Update Dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -228,6 +228,7 @@ function Dashboard() {
                 >
                   <Card.Body>
                     <Card.Title>{game.gameName}</Card.Title>
+                    <Card.Subtitle>{game.gameId}</Card.Subtitle>
                   </Card.Body>
 
                 </Card>
