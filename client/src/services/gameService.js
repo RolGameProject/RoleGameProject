53146b6b230b5fa4b4437ea553b487e0c2513be4
RolGameProject <rolgameproject@gmail.com>
2024-12-05 10:57:31 +0100

    Update gameService.js

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -52,10 +52,10 @@ export const joinGame = async (gameData) => {
       console.error('Usuario no autenticado');
       throw new Error('Usuario no autenticado');
     }
-
+    console.log('user en servicio: ', user);
     const playerId = user.userId;
 
-    console.log('Datos enviados al servidor para unirse:', { ...gameData, playerId }); // Log de los datos enviados
+    console.log('Datos enviados al servidor para unirse en servicio:', { ...gameData, playerId }); // Log de los datos enviados
     
     const response = await axios.post(
       `${API_URL}/api/games/join`,
