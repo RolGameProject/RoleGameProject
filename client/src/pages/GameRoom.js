26e578431837cccd2e70ff0a6e7398c9a40aa9a1
RolGameProject <rolgameproject@gmail.com>
2024-12-06 14:50:18 +0100

    Update GameRoom.js

--- client/src/pages/GameRoom.js
+++ client/src/pages/GameRoom.js
@@ -190,7 +190,7 @@ const fetchAllData = useCallback(async () => {
                 <div key={character.id}>
                   {gameDetails.characters.length > 0 ? (
                     <EntityCardList
-                      entities={/*gameDetails.*/characters}  // Pasamos los personajes directamente
+                      entities={character}  // Pasamos los personajes directamente
                       entityType="character"
                       onEntitySelect={setSelectedCharacter} // Establecer personaje seleccionado
                     />
