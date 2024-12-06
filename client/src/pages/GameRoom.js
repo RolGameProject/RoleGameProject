81f0902147b86b269b2f16cb15809352bcda35be
RolGameProject <rolgameproject@gmail.com>
2024-12-06 09:48:54 +0100

    Update GameRoom.js

--- client/src/pages/GameRoom.js
+++ client/src/pages/GameRoom.js
@@ -86,6 +86,7 @@ const fetchAllData = useCallback(async () => {
 
   const handleCreateEnemy = async (enemyData) => {
     try {
+      console.log('user en gameRoom createEnemy: ', user);
       console.log('enemyData en GameRoom antes de enviar: ', enemyData);
       const responseCreate = await axios.post('/api/enemies', enemyData, {
       withCredentials: true,
